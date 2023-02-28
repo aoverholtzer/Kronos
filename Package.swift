74bfff25b494baa22c3825ee801597f422fb8c26
aoverholtzer <aoverholtzer@users.noreply.github.com>
2023-02-27 19:12:11 -0800

    add catalyst support

--- Package.swift
+++ Package.swift
@@ -3,6 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "Kronos",
+    platforms: [
+        .macOS(.v10_13),
+        .tvOS(.v11),
+        .iOS(.v11)
+    ],
     products: [
         .library(name: "Kronos", targets: ["Kronos"]),
     ],
